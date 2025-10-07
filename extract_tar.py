b24723079da2cf81c7adf937b942d9d2f785ee80
Joseph <162514273+josephletobar@users.noreply.github.com>
2025-10-07 14:22:52 -0400

    Extract raw GEO dataset from GSE253975 archive

---
+++ extract_tar.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import tarfile
+
+# path to your tar file
+tar_path = "GSE253975_RAW.tar"
+
+# open and inspect
+with tarfile.open(tar_path, "r") as tar:
+    print(tar.getnames())  # lists all contained files
+
+# extract contained files
+with tarfile.open(tar_path, "r") as tar:
+    tar.extractall("GSE253975_data")  # creates a new folder with the contents
